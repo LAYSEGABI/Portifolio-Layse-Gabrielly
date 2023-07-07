f6e290fb9cb7b45085c91b89f6fb7818e516e7ad
LAYSEGABI <laysegabrielly13@gmail.com>
2023-07-07 11:18:33 -0300

    animacao de entrada e saida do conteudo na section

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
+
+// Menu hamburguer
 function menuOnClick() {
-    document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+
+// Header trocar o background color ao descer a pagina
+const desiredElement = document.getElementById('header');
+const pixelsAmount = '50';
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > pixelsAmount) {
+    desiredElement.classList.add('changeStyle');
+  } else {
+    desiredElement.classList.remove('changeStyle');
   }
+});
+
+
+//Animacao de entrada e saida de conteudo ao rolar a pagina
+const elementos = document.querySelectorAll('[data-anima]');
+const animacaoClass = 'animacao';
+
+function animaScroll() {
+  const topoPaginaNaJanela = window.pageYOffset + ((window.innerHeight * 3 / 4));
+  elementos.forEach(function (elemento) {
+    if (topoPaginaNaJanela > elemento.offsetTop) {
+      elemento.classList.add(animacaoClass);
+    } else {
+      elemento.classList.remove(animacaoClass);
+    }
+});
+}
+
+if(elementos.length){
+  window.addEventListener('scroll', function(){
+    animaScroll()
+  })
+}
